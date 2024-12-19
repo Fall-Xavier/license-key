3f35cf3e9403dadaed37565e7c672fa3eec794ee
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-12-19 22:27:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-LICENSE_FILE = "/tmp/licenses.json"
+LICENSE_FILE = "tmp/licenses.json"
 
 def get_key_from_device():
     hostname = platform.node()
